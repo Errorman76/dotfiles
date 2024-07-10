a04e240d89ad152e566b508a147e1b0931900b9c
Errorman76 <lwooyoung57@gmail.com>
2024-07-10 22:59:14 +0900

    [update] update bufferline by default setting

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,9 +1,20 @@
 return {
-  "akinsho/bufferline.nvim",
+  'akinsho/bufferline.nvim',
   version = "*",
-  opts = {
-    options = {
-      mode = "buffers",
-    },
-  },
+  dependencies = 'nvim-tree/nvim-web-devicons',
+  config = function ()
+    local bufferline = require("bufferline")
+    bufferline.setup({
+      options = {
+        mode = "buffers",
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "File Explorer",
+            separator = true -- use a "true" to enable the default, or set your own character
+          },
+        },
+      },
+    })
+  end,
 }

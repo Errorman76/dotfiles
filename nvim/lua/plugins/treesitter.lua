eed49acd2cef85789f72a9e68af5fbcd504e559d
Errorman76 <lwooyoung57@gmail.com>
2024-07-17 21:19:10 +0900

    [update] nvim-treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,23 +1,11 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
   build = ":TSUpdate",
   config = function()
-    -- import nvim-treesitter plugin
-    local treesitter = require("nvim-treesitter.configs")
-
-    -- configure treesitter
-    treesitter.setup({ -- enable syntax highlighting
-      highlight = {
-        enable = true,
-      },
-      -- enable indentation
+    require("nvim-treesitter.configs").setup({
+      auto_install = true,
+      highlight = { enable = true },
       indent = { enable = true },
-      -- enable autotagging (w/ nvim-ts-autotag plugin)
-      autotag = {
-        enable = true,
-      },
-      -- ensure these language parsers are installed
       ensure_installed = {
         "json",
         "javascript",
@@ -40,12 +28,12 @@ return {
       },
       incremental_selection = {
         enable = true,
-        -- keymaps = {
-        --   init_selection = "<C-space>",
-        --   node_incremental = "<C-space>",
-        --   scope_incremental = false,
-        --   node_decremental = "<bs>",
-        -- },
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
       },
     })
   end,

3d2db79867b52345cc1fb884cf6b2bf63531082e
Errorman76 <lwooyoung57@gmail.com>
2024-07-19 21:12:46 +0900

    [update] lauline: s

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,7 +2,7 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = {
     "nvim-tree/nvim-web-devicons",
-    "arkav/lualine-lsp-progress",
+    "nvim-lua/lsp-status.nvim",
   },
   config = function()
     require("lualine").setup({
@@ -11,9 +11,45 @@ return {
         globalstatus = true,
       },
       sections = {
-        lualine_c = {
-          { "filename" },
-          { "lsp_progress" },
+        lualine_x = {
+          {
+            require("lazy.status").updates,
+            cond = require("lazy.status").has_updates,
+            color = { fg = "#ff9e64" },
+          },
+          {
+            -- display all attahced clients
+            function()
+              local clients = vim.lsp.get_active_clients()
+              if next(clients) == nil then
+                return ''
+              end
+
+              local clients_list = {}
+              for _, client in pairs(clients) do
+                table.insert(clients_list, client.name)
+              end
+
+              return '\u{f085} ' .. table.concat(clients_list, "|")
+            end
+
+            -- Display only the lsp clients associated with the current buffer
+            -- function ()
+            --   local clients = vim.lsp.buf_get_clients(vim.api.nvim_get_current_buf())
+            --   if next(clients) == nil then
+            --     return ''
+            --   end
+            --
+            --   local c = {}
+            --   for _, client in pairs(clients) do
+            --     table.insert(c, client.name)
+            --   end
+            --   return '\u{f085} ' .. table.concat(c, '|')
+            -- end
+          },
+          { 'encoding' },
+          { 'fileformat' },
+          { 'filetype' },
         },
       },
     })

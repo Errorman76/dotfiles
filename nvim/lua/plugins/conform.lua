3a3fa6b5f14e197412003c38359891e09353fffe
Errorman76 <lwooyoung57@gmail.com>
2024-07-22 18:04:31 +0900

    [feat] conform and re format bufferline, indnet-blankline

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -1,4 +1,32 @@
--- TODO: formatting 
-if true then return {} end
+return {
+  "stevearc/conform.nvim",
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        markdown = { "prettier" },
 
+        sh = { "shfmt" },
+        lua = { "stylua" },
+        python = { "isort", "black" },
+        go = { "goimports_reviser", "gofmt", "golines" },
 
+        javascript = { "prettier" },
+        typescript = { "prettier" },
+        javascriptreact = { "prettier" },
+        typescriptreact = { "prettier" },
+
+        css = { "prettier" },
+        scss = { "prettier" },
+        html = { "prettier" },
+        json = { "prettier" },
+
+        yaml = { "prettier" },
+        toml = { "taplo" },
+      },
+      format_on_save = {
+        lsp_format = "fallback",
+        timeout_ms = 500,
+      },
+    })
+  end,
+}

de8de0ea09332532aa878394a8a4b3b52158e636
Errorman76 <lwooyoung57@gmail.com>
2024-07-25 22:20:04 +0900

    [update] adapt formatter to utils.lua

--- nvim/lua/core/utils.lua
+++ nvim/lua/core/utils.lua
@@ -2,41 +2,41 @@ local M = {}
 
 -- Get lsp servers
 function M.get_attached_clients()
-  local bufnr = vim.api.nvim_get_current_buf()
-  local clients = vim.lsp.buf_get_clients(bufnr)
-  if next(clients) == nil then
-    return ''
-  end
+	local bufnr = vim.api.nvim_get_current_buf()
+	local clients = vim.lsp.buf_get_clients(bufnr)
+	if next(clients) == nil then
+		return ""
+	end
 
-  local clients_list = {}
-  for _, client in pairs(clients) do
-    table.insert(clients_list, client.name)
-  end
+	local clients_list = {}
+	for _, client in pairs(clients) do
+		table.insert(clients_list, client.name)
+	end
 
-  return '\u{eaf2} ' .. table.concat(clients_list, "|")
+	return "\u{eaf2} " .. table.concat(clients_list, "|")
 end
 
 -- Get formatters
 function M.get_attached_formatters()
-  local status, conform = pcall(require, 'conform')
-  if not status then
-    return 'Conform not installed'
-  end
+	local status, conform = pcall(require, "conform")
+	if not status then
+		return "Conform not installed"
+	end
 
-  local bufnr = vim.api.nvim_get_current_buf()
-  local formatters = conform.list_formatters(bufnr)
+	local bufnr = vim.api.nvim_get_current_buf()
+	local formatters = conform.list_formatters(bufnr)
 
-  if #formatters > 0 then
-    local formatter_names = {}
+	if #formatters > 0 then
+		local formatter_names = {}
 
-    for _, formatter in ipairs(formatters) do
-      table.insert(formatter_names, formatter.name)
-    end
+		for _, formatter in ipairs(formatters) do
+			table.insert(formatter_names, formatter.name)
+		end
 
-    return '\u{eab2} ' .. table.concat(formatter_names, '|')
-  end
+		return "\u{eab2} " .. table.concat(formatter_names, "|")
+	end
 
-  return ''
+	return ""
 end
 
 return M

db7ddc793f08d101f4795684e94e1486db2e846e
Errorman76 <lwooyoung57@gmail.com>
2024-07-30 22:11:21 +0900

    [style] rename file

---
+++ nvim/lua/plugins/nvim-dap-ui.lua
@@ -0,0 +1,31 @@
+return {
+	"rcarriga/nvim-dap-ui",
+	dependencies = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" },
+	config = function()
+		require("dapui").setup({
+			controls = {
+				enabled = true,
+				element = "repl",
+				icons = {
+					pause = "",
+					play = "",
+					step_into = "",
+					step_over = "",
+					step_out = "",
+					step_back = "",
+					run_last = "",
+					terminate = "",
+				},
+			},
+		})
+
+		local dap, dapui = require("dap"), require("dapui")
+
+    -- stylua: ignore start
+		dap.listeners.before.attach.dapui_config = function() dapui.open() end
+		dap.listeners.before.launch.dapui_config = function() dapui.open() end
+		dap.listeners.before.event_terminated.dapui_config = function() dapui.close() end
+		dap.listeners.before.event_exited.dapui_config = function() dapui.close() end
+		-- stylua: ignore end
+	end,
+}

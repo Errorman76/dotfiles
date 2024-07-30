fc2507d99cf95db45b090cbbd830be0542ba360f
Errorman76 <lwooyoung57@gmail.com>
2024-07-30 21:54:54 +0900

    [update] detach mason from lsp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -3,80 +3,32 @@ return {
 	dependencies = {
 		"williamboman/mason.nvim",
 		"williamboman/mason-lspconfig",
-		"WhoIsSethDaniel/mason-tool-installer.nvim",
 
 		"hrsh7th/cmp-nvim-lsp",
 	},
 	config = function()
-		require("mason").setup()
-		require("mason-lspconfig").setup({
-			ensure_installed = {
-				-- bash
-				"bashls",
-
-				-- lua
-				"lua_ls",
-
-				-- frontend
-				"tsserver",
-				"html",
-				"cssls",
-				"tailwindcss",
-				"jsonls",
-
-				-- python
-				"pyright",
-
-				-- go
-				-- "gopls",
-
-				-- rust
-				"rust_analyzer",
-
-				-- nginx
-				-- "nginx_language_server",
-
-				-- etc
-				"dockerls",
-				"taplo",
-				-- "yamllsk"
-			},
-		})
-
-		require("mason-tool-installer").setup({
-			ensure_installed = {
-				-- formatter
-				"prettier",
-				"black",
-				"isort",
-				"shfmt",
-				"stylua",
-
-				-- linter
-				"eslint_d",
-				"pylint",
-			},
-		})
-
 		-- The servers and set up each one with the same capabilities
+		local lspconfig = require("lspconfig")
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
 		require("mason-lspconfig").setup_handlers({
 			function(server_name)
-				require("lspconfig")[server_name].setup({
-					capabilities = require("cmp_nvim_lsp").default_capabilities(),
+				lspconfig[server_name].setup({
+					capabilities = capabilities,
 				})
 			end,
-			["lua_ls"] = function()
-				require("lspconfig").lua_ls.setup({
-					capabilities = require("cmp_nvim_lsp").default_capabilities(),
-					settings = {
-						Lua = {
-							diagnostics = {
-								globals = { "vim" },
-							},
-						},
+		})
+
+		-- lus_ls setup
+		lspconfig.lua_ls.setup({
+			capabilities = capabilities,
+			settings = {
+				Lua = {
+					diagnostics = {
+						globals = { "vim" },
 					},
-				})
-			end,
+				},
+			},
 		})
 	end,
 }
---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,63 @@
+return {
+	"williamboman/mason.nvim",
+	dependencies = {
+		"williamboman/mason-lspconfig",
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		"jay-babu/mason-nvim-dap.nvim",
+	},
+	config = function()
+		require("mason").setup()
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				-- bash
+				"bashls",
+
+				-- lua
+				"lua_ls",
+
+				-- frontend
+				"tsserver",
+				"html",
+				"cssls",
+				"tailwindcss",
+				"jsonls",
+
+				-- python
+				"pyright",
+
+				-- go
+				-- "gopls",
+
+				-- rust
+				"rust_analyzer",
+
+				-- nginx
+				-- "nginx_language_server",
+
+				-- etc
+				"dockerls",
+				"taplo",
+				-- "yamllsk"
+			},
+		})
+
+		require("mason-tool-installer").setup({
+			ensure_installed = {
+				-- formatter
+				"prettier",
+				"black",
+				"isort",
+				"shfmt",
+				"stylua",
+
+				-- linter
+				"eslint_d",
+				"pylint",
+			},
+		})
+
+		require("mason-nvim-dap").setup({
+			ensure_installed = {},
+		})
+	end,
+}

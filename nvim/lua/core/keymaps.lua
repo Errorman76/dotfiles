bdbe9c324fa491a23a999d10e8143bf25809b50c
Errorman76 <lwooyoung57@gmail.com>
2024-07-31 17:50:35 +0900

    [update] diff keymap

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -55,9 +55,9 @@ map("n", "<leader>se", "<C-w>=", { desc = "equalize window size" })
 -- diff
 -- stylua: ignore start
 map("n", "<leader>df", function() utils_diff.telescope_diff_file() end, { desc = "Diff file with current buffer" })
-map("n", "<leader>dr", function() utils_diff.telescope_diff_file(true) end, { desc = "Diff recent file with current buffer" })
-map("n", "<leader>dg", function() utils_diff.telescope_diff_from_history() end, { desc = "Diff from git history" })
 map("v", "<leader>dc", function() utils_diff.diff_from_clipboard() end, { desc = "Diff from clipboard" })
+-- map("n", "<leader>dr", function() utils_diff.telescope_diff_file(true) end, { desc = "Diff recent file with current buffer" })
+-- map("n", "<leader>dg", function() utils_diff.telescope_diff_from_history() end, { desc = "Diff from git history" })
 -- stylua: ignore end
 
 -- Code/LSP

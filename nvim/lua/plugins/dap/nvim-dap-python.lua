82624a038162bd8cc0b5c983bf65fc5df1b31e2c
Errorman76 <lwooyoung57@gmail.com>
2024-08-06 17:18:28 +0900

    [feat] nvim-dap-python

--- nvim/lua/plugins/dap/nvim-dap-python.lua
+++ nvim/lua/plugins/dap/nvim-dap-python.lua
@@ -1,22 +1,11 @@
-if true then
-	return {}
-end
-
 return {
 	"mfussenegger/nvim-dap-python",
 	dependencies = "mfussenegger/nvim-dap",
-	ft = "python", -- NOTE: ft: lazy-load on filetype
-	config = function(_, opts)
-		local path = vim.fn.exepath("python")
+	ft = "python",
+	config = function()
 		local debugpy = require("mason-registry").get_package("debugpy")
-		if debugpy:is_installed() then
-			path = debugpy:get_install_path()
-			if vim.fn.has("win32") == 1 then
-				path = path .. "/venv/Scripts/python"
-			else
-				path = path .. "/venv/bin/python"
-			end
-		end
-		require("dap-python").setup(path, opts)
+		local path = debugpy:get_install_path() .. "/venv/bin/python"
+
+		require("dap-python").setup(path)
 	end,
 }

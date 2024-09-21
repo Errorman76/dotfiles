dfaba675b2468863e32a498bdd20997f41d25904
Errorman76 <lwooyoung57@gmail.com>
2024-09-21 18:24:43 +0900

    [update] remove bufferline option that seperate, that is default option

--- nvim/lua/plugins/bufferline.lua
+++ nvim/lua/plugins/bufferline.lua
@@ -1,20 +1,19 @@
 return {
-  'akinsho/bufferline.nvim',
-  version = "*",
-  dependencies = 'nvim-tree/nvim-web-devicons',
-  config = function()
-    local bufferline = require("bufferline")
-    bufferline.setup({
-      options = {
-        mode = "buffers",
-        offsets = {
-          {
-            filetype = "neo-tree",
-            text = "File Explorer",
-            separator = true -- use a "true" to enable the default, or set your own character
-          },
-        },
-      },
-    })
-  end,
+	"akinsho/bufferline.nvim",
+	version = "*",
+	dependencies = "nvim-tree/nvim-web-devicons",
+	config = function()
+		local bufferline = require("bufferline")
+		bufferline.setup({
+			options = {
+				mode = "buffers",
+				offsets = {
+					{
+						filetype = "neo-tree",
+						text = "File Explorer",
+					},
+				},
+			},
+		})
+	end,
 }

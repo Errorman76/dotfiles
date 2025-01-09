aad48e049ed47b782e721232d162e2ff1edb50b0
Errorman76 <lwooyoung57@gmail.com>
2025-01-09 11:19:05 +0900

    [update] bakcup nvim directory

--- nvim/lua/plugins/diffview.lua
+++
@@ -1,7 +0,0 @@
-return {
-	"sindrets/diffview.nvim",
-	keys = {
-		{ "<leader>dvo", "<cmd>DiffviewOpen<CR>", desc = "diffview open" },
-		{ "<leader>dvc", "<cmd>DiffviewClose<CR>", desc = "diffview close" },
-	},
-}
--- nvim/lua/plugins/vim-maximizer.lua
+++
@@ -1,6 +0,0 @@
-return {
-	"szw/vim-maximizer",
-	keys = {
-		{ "<leader>pm", "<cmd>MaximizerToggle<CR>", "n", desc = "mximize current pane" },
-	},
-}
